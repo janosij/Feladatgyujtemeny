07fa6ebb60f7719a30bff28dd0acea1323c72e3a
janosij <janosij12@gmail.com>
2024-06-17 12:27:58 +0200

    újragondolás

--- JS/Tombfuggvenyek/Filter/fv.js
+++ JS/Tombfuggvenyek/Filter/fv.js
@@ -80,27 +80,38 @@ console.log(number.filter(x=>x%2!==0 && x>0 && x<10));
 
 /*
   7.  sor:  A FV-nek az "x" nevű paraméterébe adjuk át a vizsgálandó tömböt!!! 
+
   9.  sor:  először létrehozunk egy üres tömböt, ebben tároljuk majd az eredményt 
+
   10. sor:  végiglépkedünk a paraméterben átadott tömbön, ha páros, akkor beletesszük 
             az itt létrehozott tömbbe 
+
   12. sor:  Az elágazásban megnézem, hogy 2-vel osztva 0-át ad-e maradékul az adott elem
+
   14. sor:  A PUSH utasítással beszúrom az aktuális TÖMB végére a paraméterben megadott elemet
+
   18. sor:  a ciklus lefutása után visszaadjuk a hívás helyére az új tömböt, 
             ami csak a páros számokat tartalmazza
+
   20. sor:  A CONSOLE ablakba kiírjuk az eredményt 
             (böngészőben nézem a HTML-t F12 gomb és console fül)
+
   25. sor:  A FV-nek az Y nevű paraméterébe adjuk át a vizsgálandó tömböt!!! 
+
   27. sor:  Itt megint "even" lett az üres tömb neve. Ilyet meg egyszer már deklaráltunk! 
             FONTOS, hogy ez a deklaráció csak ebben a függvényben létezik! 
             NEM zavarja, hogy a másik fv-ben pont ezzel a névvel már létrehoztam egy tömböt!
             Az elsőnek létrehozott csak az ELSŐ FV-ben létezik! A HATÓKÖRE csak ott él!
+
   29. sor:  A végiglépkedést NEM AZ INDEXEKEN csinálom, mint az előbb, 
             hanem az OF segítségével lépkedek végig minden tömbelemen,
             ilyenkor az X változó felveszi a tömb minden elemének az értékét,
             olyan, mint a FOREACH a C típsú nyelvekben!!!
+
   51. sor:  Itt vizsgáljuk, hogy ha a megadott FV megkapja az aktuális tömbelemet, 
             mint paramétert, akkor IGAZZAL vagy HAMISSAL tér-e vissza. 
             Ha IGAZ a visszatérési érték, akkor beszúrja az új tömb végére
+            
   67. sor:  Most lássuk, miért jó ez a +FV a meghívásnál:
             tegyük fel, hogy most a pozitív számokat szeretnék kiválogatni.
             Ebben az esetben CSAK a HÍVÁS szintaxisán KELL MÓDOSÍTANI
