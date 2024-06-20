d3a91aad8737bd1c11a2947ed6b76a43c21d4fef
janosij <janosij12@gmail.com>
2024-06-20 17:22:29 +0200

    ujragondolas

---
+++ JS/Stringműveletek/02-Nagy kezdőbetűk/nagy.js
@@ -0,0 +1,33 @@
+// Alakítsuk át a sztring első karakterét nagybetűssé
+
+let nev= "dezső";
+nev = nev.charAt(0).toUpperCase() + nev.slice(1);
+console.log(nev);
+
+// Oldjuk meg függvények segítségével a feladatot
+console.log("------------------");
+nev ="rozál"
+
+// Itt azt használjuk ki, hogy a sztring valójában karakterek tömbje
+function nagyKezdo1(string) {
+    return string[0].toUpperCase() + string.slice(1);
+}
+
+// Ez már az ismert megoldás
+function nagyKezdo2(string) {
+    return string.charAt(0).toUpperCase() + string.slice(1);
+}
+
+// Az érdekesség kedvéért még idetettem 2 másik megoldást is
+function nagyKezdo3(string) {
+    return string.replace(/^./, string[0].toUpperCase());
+}
+
+String.prototype.nagyKezdo4 = function() {
+    return this.charAt(0).toUpperCase() + this.slice(1);
+}
+
+console.log(nagyKezdo1(nev));
+console.log(nagyKezdo2(nev));
+console.log(nagyKezdo3(nev));
+console.log(nev.nagyKezdo4());
